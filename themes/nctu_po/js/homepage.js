cc4e88404eb95577a8e1db984649c7dda6389c53
candiceliu0421 <candice102403006@gmail.com>
2018-04-04 14:30:58 +0800

    bottleneck

--- themes/nctu_po/js/homepage.js
+++ themes/nctu_po/js/homepage.js
@@ -1,101 +1,17 @@
 // nav hover 展開2個直角梯形
 $(document).ready(function () {
     $('div.top').hover(function () {
-        $(this).find('div.navbox1').show();
+
+        $(this).find('div[class^="navbox"]').show();
+        $(this).addClass("hover");
+
+
     }, function () {
-        $(this).find('div.navbox1').hide();
+
+        $(this).find('div[class^="navbox"]').hide();
+        $(this).removeClass("hover");
     });
-});
-$(document).ready(function () {
-    $('div.top').hover(function () {
-        $(this).find('div.navbox1_2').show();
-    }, function () {
-        $(this).find('div.navbox1_2').hide();
-    });
-});
-$(document).ready(function () {
-    $('div.top').hover(function () {
-        $(this).find('div.navbox2').show();
-    }, function () {
-        $(this).find('div.navbox2').hide();
-    });
-});
-$(document).ready(function () {
-    $('div.top').hover(function () {
-        $(this).find('div.navbox2_2').show();
-    }, function () {
-        $(this).find('div.navbox2_2').hide();
-    });
-});
-$(document).ready(function () {
-    $('div.top').hover(function () {
-        $(this).find('div.navbox3').show();
-    }, function () {
-        $(this).find('div.navbox3').hide();
-    });
-});
-$(document).ready(function () {
-    $('div.top').hover(function () {
-        $(this).find('div.navbox3_2').show();
-    }, function () {
-        $(this).find('div.navbox3_2').hide();
-    });
-});
-$(document).ready(function () {
-    $('div.top').hover(function () {
-        $(this).find('div.navbox4').show();
-    }, function () {
-        $(this).find('div.navbox4').hide();
-    });
-});
-$(document).ready(function () {
-    $('div.top').hover(function () {
-        $(this).find('div.navbox4_2').show();
-    }, function () {
-        $(this).find('div.navbox4_2').hide();
-    });
-});
-$(document).ready(function () {
-    $('div.top').hover(function () {
-        $(this).find('div.navbox5').show();
-    }, function () {
-        $(this).find('div.navbox5').hide();
-    });
-});
-$(document).ready(function () {
-    $('div.top').hover(function () {
-        $(this).find('div.navbox5_2').show();
-    }, function () {
-        $(this).find('div.navbox5_2').hide();
-    });
-});
-$(document).ready(function () {
-    $('div.top').hover(function () {
-        $(this).find('div.navbox6').show();
-    }, function () {
-        $(this).find('div.navbox6').hide();
-    });
-});
-$(document).ready(function () {
-    $('div.top').hover(function () {
-        $(this).find('div.navbox6_2').show();
-    }, function () {
-        $(this).find('div.navbox6_2').hide();
-    });
-});
-$(document).ready(function () {
-    $('div.top').hover(function () {
-        $(this).find('div.navbox7').show();
-    }, function () {
-        $(this).find('div.navbox7').hide();
-    });
-});
-$(document).ready(function () {
-    $('div.top').hover(function () {
-        $(this).find('div.navbox7_2').show();
-    }, function () {
-        $(this).find('div.navbox7_2').hide();
-    });
+
 });
 
 // $(document).ready(function(){
--- themes/nctu_po/page-templates/template-intro.php
+++ themes/nctu_po/page-templates/template-intro.php
@@ -11,6 +11,10 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewpoint" content="width=device-width, initial-scale=1" />
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<meta http-equiv="expires" content="0">
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="Cathe-Control" content="no-cache">
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <!-- bootstrap -->
 <link href="<?php bloginfo('template_url'); ?>/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -19,7 +23,7 @@
 <link href="<?php bloginfo('template_url'); ?>/css/intro.css" rel="stylesheet">
 <script src="<?php bloginfo('template_url'); ?>/js/intro.js"></script>
 
-  <style type="text/css"></style>
+<style type="text/css"></style>
 </head>
 <script type="text/javascript">
   
@@ -36,17 +40,15 @@
     
     
 <div class="top" id="top_1">
-  <div id="nav1">
-    <div id="navid1">關於本室</div>
-  </div>
+  <a href="<?php echo site_url(); ?>/homepage/"><div id="nav1"><div id="navid1">關於本室</div></div></a>
   <div class="navbox1_3">   <!-- 點擊展開出左大梯形 -->
     <div id="navboxw1_3">
       可 查 閱 本 室 簡 介 、 本<br>室 位 置 、 主 任 簡 介 、<br>本 室 願 景 、 組 織 架<br>構 、 人 員 職 掌 及 工 作<br>成 果 等 資 訊
     </div>
-      <a href="<?php echo site_url(); ?>/intro/"><div id="navboxw1_3_1">本 室 簡 介<br></div></a>
-      <div id="navboxw1_3_2">本 室 位 置<br></div>
+      <a class="refresh_link" href="<?php echo site_url(); ?>/intro/"><div id="navboxw1_3_1">本 室 簡 介<br></div></a>
+      <a href="<?php echo site_url(); ?>/site/"><div id="navboxw1_3_2">本 室 位 置<br></div></a>
       <div id="navboxw1_3_3">人 員 職 掌<br></div>
-      <div id="navboxw1_3_4">本 室 願 景<br></div>
+      <a href="<?php echo site_url(); ?>/vision/"><div id="navboxw1_3_4">本 室 願 景<br></div></a>
       <div id="navboxw1_3_5">工 作 成 果</div>
   </div>
   <div class="navbox1_4">   <!-- 點擊展開出右大梯形 -->
@@ -123,4 +125,9 @@
 
 
 </body>
+
+<script type="text/javascript">
+  
+  
+</script>
 </html>
