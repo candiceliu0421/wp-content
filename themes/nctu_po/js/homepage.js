0d98257ac4f913304a13c031ea7d3346053a07cf
candiceliu0421 <candice102403006@gmail.com>
2018-06-02 16:12:39 +0800

    上方橫幅hover+點擊都行

--- themes/nctu_po/js/homepage.js
+++ themes/nctu_po/js/homepage.js
@@ -17,28 +17,27 @@ $(document).ready(function () {
 
 
 // nav click 展開2個直角梯形
-// $(document).ready(function () {
-//     $('div.top').click(function () {
-//     	// 點擊別的不會衝突 
-//     	sibling = $(this).siblings(".click1")
-//     	if (sibling){
-// 			sibling.find('div[class^="navbox"]').hide();
-// 			sibling.removeClass("click1");
-//     	    sibling.find('div[id^="nav"]').removeClass("click1");
-//     	}
-//     	// 點擊show&hide
-//     	if ($(this).hasClass("click1")) {
-// 	        $(this).find('div[class^="navbox"]').hide();
-//     	    $(this).removeClass("click1");
-//     	    $(this).find('div[id^="nav"]').removeClass("click1");
-//     	} else {
-// 	        $(this).find('div[class^="navbox"]').show();
-// 	        $(this).addClass("click1");
-//     	    $(this).find('div[id^="nav"]').addClass("click1");
-//     	}
-//     });
-
-// });
+$(document).ready(function () {
+    $('div.top').click(function () {
+    	// 點擊別的不會衝突 
+    	sibling = $(this).siblings(".click1")
+    	if (sibling){
+			sibling.find('div[class^="navbox"]').hide();
+			sibling.removeClass("click1");
+    	    sibling.find('div[id^="nav"]').removeClass("click1");
+    	}
+    	// 點擊show&hide
+    	if ($(this).hasClass("click1")) {
+	        $(this).find('div[class^="navbox"]').hide();
+    	    $(this).removeClass("click1");
+    	    $(this).find('div[id^="nav"]').removeClass("click1");
+    	} else {
+	        $(this).find('div[class^="navbox"]').show();
+	        $(this).addClass("click1");
+    	    $(this).find('div[id^="nav"]').addClass("click1");
+    	}
+    });
+});
 
 
 
